0499ff7bdff9a491659f3db8d68281c8ea53a425
Gabriel Gregório <gabriel.gregorio@pravaler.com.br>
2023-03-30 09:59:03 -0300

    add search system

--- src/analyzer.ts
+++ src/analyzer.ts
@@ -57,45 +57,48 @@ export class Compare {
     return [targetWords.join(' ').trim(), inputWorlds.join(' ').trim()];
   }
 
-  public static analyzeLetters(inputNormalized: string, targetNormalized: string): number {
+  public static analyzeLetters(inputNormalized: string, targetNormalized: string): { percent: number; rank: number } {
     const inputLetters = inputNormalized.replaceAll(/\s/g, '').split('');
     const targetLetters = targetNormalized.replaceAll(/\s/g, '').split('');
 
     const sizeTarget = targetLetters.length;
 
+    let countRank = 0;
     inputLetters.forEach((letter) => {
       const indexOfTarget = targetLetters.indexOf(letter);
       if (indexOfTarget >= 0) {
+        countRank += 1;
         targetLetters.splice(indexOfTarget, 1);
       }
     });
 
     try {
-      return (HALF / sizeTarget) * (sizeTarget - targetLetters.length);
+      return { percent: (HALF / sizeTarget) * (sizeTarget - targetLetters.length), rank: countRank };
     } catch (error: unknown) {
       console.error('error on letters process', error);
-      return 0;
+      return { percent: 0, rank: 0 };
     }
   }
 
-  public static analyzeWords(inputNormalized: string, targetNormalized: string): number {
+  public static analyzeWords(inputNormalized: string, targetNormalized: string): { percent: number; rank: number } {
     const inputWords = inputNormalized.split(' ');
     const targetWords = targetNormalized.split(' ');
 
     const sizeTarget = targetWords.length;
-
+    let countRank = 0;
     inputWords.forEach((world) => {
       const indexOfTarget = targetWords.indexOf(world);
       if (indexOfTarget >= 0) {
+        countRank += 1;
         targetWords.splice(indexOfTarget, 1);
       }
     });
 
     try {
-      return (HALF / sizeTarget) * (sizeTarget - targetWords.length);
+      return { percent: (HALF / sizeTarget) * (sizeTarget - targetWords.length), rank: countRank };
     } catch (error: unknown) {
       console.error('error on worlds process', error);
-      return 0;
+      return { percent: 0, rank: 0 };
     }
   }
 
@@ -109,6 +112,19 @@ export class Compare {
 
     [input, target] = this.normalizeSentences(input, target);
 
-    return fixNumber(Compare.analyzeLetters(input, target) + Compare.analyzeWords(input, target));
+    return fixNumber(Compare.analyzeLetters(input, target).percent + Compare.analyzeWords(input, target).percent);
+  }
+
+  searchRanking(inputUser: string, targetUser: string): number {
+    let input = normalizeUtf8(inputUser);
+    let target = normalizeUtf8(targetUser);
+
+    if (!input || !target) {
+      return 0;
+    }
+
+    [input, target] = this.normalizeSentences(input, target);
+
+    return fixNumber(Compare.analyzeLetters(input, target).rank + Compare.analyzeWords(input, target).rank * 4);
   }
 }
--- src/test.spec.ts
+++ src/test.spec.ts
@@ -2,18 +2,18 @@ import { Compare } from './analyzer';
 
 describe('extractors', () => {
   it('should analyze letters', async () => {
-    expect(Compare.analyzeLetters('abc', 'abc')).toEqual(50);
-    expect(Compare.analyzeLetters('cab', 'abc')).toEqual(50);
-    expect(Compare.analyzeLetters('cab', 'abca')).toEqual(37.5);
-    expect(Compare.analyzeLetters('cab', 'xabca')).toEqual(30);
-    expect(Compare.analyzeLetters('cab', 'skw')).toEqual(0);
+    expect(Compare.analyzeLetters('abc', 'abc')).toEqual({ percent: 50, rank: 3 });
+    expect(Compare.analyzeLetters('cab', 'abc')).toEqual({ percent: 50, rank: 3 });
+    expect(Compare.analyzeLetters('cab', 'abca')).toEqual({ percent: 37.5, rank: 3 });
+    expect(Compare.analyzeLetters('cab', 'xabca')).toEqual({ percent: 30, rank: 3 });
+    expect(Compare.analyzeLetters('cab', 'skw')).toEqual({ percent: 0, rank: 0 });
   });
 
   it('should analyze worlds', async () => {
-    expect(Compare.analyzeWords('abc', 'abc')).toEqual(50);
-    expect(Compare.analyzeWords('abcdk', 'abcdf')).toEqual(0);
-    expect(Compare.analyzeWords('abcdk icon', 'icon abcdf')).toEqual(25);
-    expect(Compare.analyzeWords('voce esta andando', 'andando esta voce')).toEqual(50);
+    expect(Compare.analyzeWords('abc', 'abc')).toEqual({ percent: 50, rank: 1 });
+    expect(Compare.analyzeWords('abcdk', 'abcdf')).toEqual({ percent: 0, rank: 0 });
+    expect(Compare.analyzeWords('abcdk icon', 'icon abcdf')).toEqual({ percent: 25, rank: 1 });
+    expect(Compare.analyzeWords('voce esta andando', 'andando esta voce')).toEqual({ percent: 50, rank: 3 });
   });
 
   it('should return if is synonymous', async () => {
@@ -22,12 +22,26 @@ describe('extractors', () => {
     expect(compare.isSynonymous('outro', 'preferido')).toEqual(false);
   });
 
-  it('should return if is synonymous', async () => {
+  it('should compare sentence', async () => {
     const compare = new Compare();
     expect(compare.compareSentences('favorito', 'preferido')).toEqual(100);
     expect(compare.compareSentences('meu doce predileto é abacate', 'meu doce preferido é abacate')).toEqual(100);
     expect(compare.compareSentences('o doce favorito é abacate meu bom', 'meu doce preferido é abacate')).toEqual(
       78.31,
     );
+  });
+
+  it('should search ranking', async () => {
+    const compare = new Compare();
+    expect(compare.searchRanking('favorito', 'preferido')).toEqual(12);
+    expect(compare.searchRanking('favorito', 'preferido é meu amigo')).toEqual(12);
+    expect(compare.searchRanking('favorito', 'preferido é meu amigo predileto')).toEqual(12);
+    expect(compare.searchRanking('favorito e amigo', 'preferido é meu amigo predileto')).toEqual(26);
+    expect(compare.searchRanking('favorito e amigo gelo', 'preferido é meu amigo predileto nevex')).toEqual(28);
+    expect(compare.searchRanking('favorito e amigo gelo', 'preferido é meu amigo predileto neve')).toEqual(34);
+    expect(compare.searchRanking('favorito e amigo gelo meu', 'preferido é meu amigo predileto neve')).toEqual(41);
+    expect(compare.searchRanking('item de pesquisa', 'item de pesquisa')).toEqual(26);
+    expect(compare.searchRanking('pesquisa item de ', 'item de pesquisa extra item')).toEqual(26);
+    expect(compare.searchRanking('pesquisa item de x', 'item de pesquisa extra item')).toEqual(27);
   });
 });
