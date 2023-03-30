32b2bc4267b1fb13eee942494b52162e5bfe5020
Gabriel Gregório <gabriel.gregorio@pravaler.com.br>
2023-03-30 15:57:51 -0300

    fix: tests

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,7 @@ import { Compare } from 'src/analyzer';
 const compare = new Compare();
 
 console.log(compare.compareSentences('meu doce preferido é abacaxi', 'meu doce favorito e abacaxi'));
+console.log(compare.compareSentences('meu doce preferido é abacaxi', 'meu doce favorito e abacaxi'));
+
+console.log(compare.searchRanking('meu doce preferido é abacaxi', 'meu doce favorito e abacaxi'));
+console.log(compare.searchRanking('predileto', 'meu doce favorito e abacaxi'));
